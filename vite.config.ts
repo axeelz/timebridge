b237799ddc40bb7c049dc5cd1ed2d609b0c323d6
axeelz <axelzareb@hotmail.fr>
2025-10-12 20:14:58 +0200

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [tsConfigPaths(), tanstackStart({ customViteReactPlugin: true, target: "netlify" }), viteReact()],
+  plugins: [tsConfigPaths(), tanstackStart(), netlify(), viteReact()],
 });
